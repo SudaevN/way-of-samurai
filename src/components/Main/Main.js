6dd4fa81bef47eb90ec0d5e20bcd0075eece27d5
SudaevN <nikitasud92@gmail.com>
2020-10-06 22:11:59 +0700

    Lesson 94. Add Lazy and Suspense to Chats for faster start of App and lazy loading Chats if we need it.

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,23 +4,26 @@ import {Route} from "react-router-dom";
 import DialogContainer from "./Dialog/DialogContainer";
 import ProfileContainer from "./Profile/ProfileContainer";
 import ContactsContainer from "./Contacts/ContactsContainer";
-import ChatsContainer from "./Chats/ChatsContainer";
 import UsersContainer from "./Users/UsersContainer";
 import Login from "./Login/login";
+
+const ChatsContainer = React.lazy(() => import('./Chats/ChatsContainer'));
 
 
 const Main = (props) => {
 
-    return (
-        <div className={classes.wrapMain}>
-            <Route path={'/users'} render={ () => <UsersContainer/> } />
-            <Route path={'/chats'} render={ () => <ChatsContainer /> } />
-            <Route path={'/contacts'} render={ () => <ContactsContainer /> } />
-            <Route path={'/profile/:userId?'} render={ () => <ProfileContainer /> } />
-            <Route path={'/dialog'} render={ () => <DialogContainer /> } />
-            <Route path={'/login'} render={ () => <Login/>} />
-        </div>
-    );
+  return (
+    <div className={classes.wrapMain}>
+      <Route path={'/users'} render={() => <UsersContainer/>}/>
+      <Route path={'/chats'} render={() => {
+        return <React.Suspense fallback={<div>Загрузка...</div>}><ChatsContainer/></React.Suspense>
+      }}/>
+      <Route path={'/contacts'} render={() => <ContactsContainer/>}/>
+      <Route path={'/profile/:userId?'} render={() => <ProfileContainer/>}/>
+      <Route path={'/dialog'} render={() => <DialogContainer/>}/>
+      <Route path={'/login'} render={() => <Login/>}/>
+    </div>
+  );
 };
 
 export default Main;
